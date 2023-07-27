b0d0d1687f6411047dbf939462ab29e946c89bd3
Renato Souza <renatorenos@gmail.com>
2023-07-27 16:24:18 -0300

    comentario urls.py

--- app/urls.py
+++ app/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+# define todas as rotas do projeto
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
