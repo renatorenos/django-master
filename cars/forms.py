26264d0dab559c22a497bb288b282ad1a982e5a3
Renato Souza <renatorenos@gmail.com>
2023-09-03 21:46:42 -0300

    Criado validacao de cadastro de carro

--- cars/forms.py
+++ cars/forms.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django import forms
 from cars.models import Car
 
@@ -5,3 +6,15 @@ class CarsModelFrom(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 10000:
+            self.add_error('value', 'Valor mínimo do carro deve ser maior que R$10.000')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1950:
+            self.add_error('factory_year', 'Ano do veiculo precisa ser maior que 1950')
+        return factory_year
